ed2879b32e72efd4e8f20c6a0bb9bb1f66605181
hellayush <129208635+hellayush@users.noreply.github.com>
2026-03-28 16:00:32 +0530

    feat: created sale and saleId entity classes

---
+++ src/main/java/com/cg/entity/SaleId.java
@@ -0,0 +1,39 @@
+package com.cg.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleId implements Serializable {
+    @Column(name = "stor_id", length = 4)
+    private String storId;
+
+    @Column(name = "ord_num", length = 20)
+    private String ordNum;
+
+    @Column(name = "title_id", length = 10)
+    private String titleId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SaleId)) return false;
+        SaleId that = (SaleId) o;
+        return Objects.equals(storId, that.storId) &&
+                Objects.equals(ordNum, that.ordNum) &&
+                Objects.equals(titleId, that.titleId);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(storId, ordNum, titleId);
+    }
+}
